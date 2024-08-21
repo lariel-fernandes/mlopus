6c5f47affe4ca2727e3ace82c4bedeaaf4504045
Lariel Fernandes <89975469+lariel-fernandes@users.noreply.github.com>
2024-08-21 20:14:06 +0200

    docs: fix API ref docs and workflow (#3)
    * ci(api-docs): fix step id, fix bash expressions, use checkout v4, use commitizen to get proj version
    
    * docs(api-ref): use rtd theme with proj env instead of uv tools, fix hyperlinks
    
    * docs(README.md): fix link to api ref docs

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -18,7 +18,12 @@ release = "0.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -28,5 +33,5 @@ language = "en"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
