5931dcb0e7461ff931f6c98f6a03e55a1a25e648
Lariel Fernandes <lariel.c2.fernandes@gmail.com>
2025-11-23 23:11:12 +0100

    chore: use jinja yaml conf loader in example

--- examples/2_a_kedro_project/src/mlopus_kedro_example/settings.py
+++ examples/2_a_kedro_project/src/mlopus_kedro_example/settings.py
@@ -4,16 +4,24 @@ https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 
 from kedro.framework.session import KedroSession
 
-from mlopus.kedro import MlopusConfigLoader, MlopusKedroSession, hooks
+from mlopus.kedro import JinjaYamlConfigLoader, MlopusKedroSession, hooks
 
 # Patch the Kedro session
 KedroSession.create = MlopusKedroSession.create
 
 # Patch the Kedro config loader
-CONFIG_LOADER_CLASS = MlopusConfigLoader
+CONFIG_LOADER_CLASS = JinjaYamlConfigLoader
+CONFIG_LOADER_ARGS = {
+    "namespaces": ["globals"],  # load this namespace first
+    "load_mode": "all",  # then load all other namespaces
+    "namespace_mappings": {
+        "catalog": "io",  # remap so kedro finds the data catalog configs
+        "parameters": "nodes",  # remap so the node factories find the parameters
+    }
+}
 
 # Hooks in reverse order of execution
 HOOKS = (
-    hooks.MlflowTracker.factory(),
-    hooks.MlflowArtifacts.factory(),
+    hooks.MlflowTracker.factory("hooks.mlflow_tracker"),
+    hooks.MlflowArtifacts.factory("hooks.mlflow_artifacts"),
 )
